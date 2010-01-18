d5b9e9245fe862196e0b638d7a1ce7a061876f71
rbjarnason <robert@decyphermedia.com>
2010-01-18 21:28:58 +0000

    Fixed missing check for s3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,5 @@ ENV['HOPTOAD_KEY'] = DB_CONFIG[RAILS_ENV]['hoptoad_key']
 ENV['TWITTER_LOGIN'] = DB_CONFIG[RAILS_ENV]['twitter_login']
 ENV['TWITTER_PASSWORD'] = DB_CONFIG[RAILS_ENV]['twitter_password']
 ENV['WEBSOLR_URL'] = DB_CONFIG[RAILS_ENV]['websolr_url']
-
-S3_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/s3.yml")
+S3_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/s3.yml") if File.exists?("#{RAILS_ROOT}/config/s3.yml")
 Paperclip.options[:image_magick_path] = "/opt/local/bin"
